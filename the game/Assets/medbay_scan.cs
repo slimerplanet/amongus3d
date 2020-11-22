e6d7b12c3f1afaa33cadf174135abe3f0739330b
slimerplanet <eppuku@gmail.com>
2020-11-22 14:49:49 +0200

    scan

---
+++ the game/Assets/medbay_scan.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class medbay_scan : MonoBehaviour
+{
+    public Animator animator;
+    public Icons icons;
+    bool inTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("localplayer"))
+        {
+            icons.setUSEActive(true);
+            inTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("localplayer"))
+        {
+            inTrigger = false;
+            icons.setUSEActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if(inTrigger && Input.GetKeyDown(KeyCode.E)) {
+
+        }
+    }
+}
